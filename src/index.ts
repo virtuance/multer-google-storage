6b995be13220767deb2afd0f5cf4cbed26620e73
Jared Gilpin <jared.gilpin@regiscompany.com>
2020-08-21 10:56:22 -0600

    trying to break it

--- src/index.ts
+++ src/index.ts
@@ -25,79 +25,79 @@ export default class MulterGoogleCloudStorage implements multer.StorageEngine {
 	}
 
 	constructor(opts?: ConfigurationObject & { filename?: any, bucket?: string, contentType?: ContentTypeFunction }) {
-		opts = opts || {};
+		// opts = opts || {};
 
-		this.getFilename = (opts.filename || this.getFilename);
-		this.getContentType = (opts.contentType || this.getContentType);
+		// this.getFilename = (opts.filename || this.getFilename);
+		// this.getContentType = (opts.contentType || this.getContentType);
 
-		opts.bucket = (opts.bucket || process.env.GCS_BUCKET || null);
-		opts.projectId = opts.projectId || process.env.GCLOUD_PROJECT || null;
-		opts.keyFilename = opts.keyFilename || process.env.GCS_KEYFILE || null;
+		// opts.bucket = (opts.bucket || process.env.GCS_BUCKET || null);
+		// opts.projectId = opts.projectId || process.env.GCLOUD_PROJECT || null;
+		// opts.keyFilename = opts.keyFilename || process.env.GCS_KEYFILE || null;
 
-		if (!opts.bucket) {
-			throw new Error('You have to specify bucket for Google Cloud Storage to work.');
-		}
+		// if (!opts.bucket) {
+		// 	throw new Error('You have to specify bucket for Google Cloud Storage to work.');
+		// }
 
-		if (!opts.projectId) {
-			throw new Error('You have to specify project id for Google Cloud Storage to work.');
-		}
+		// if (!opts.projectId) {
+		// 	throw new Error('You have to specify project id for Google Cloud Storage to work.');
+		// }
 
-		if (!opts.keyFilename) {
-			throw new Error('You have to specify credentials key file for Google Cloud Storage to work.');
-		}
+		// if (!opts.keyFilename) {
+		// 	throw new Error('You have to specify credentials key file for Google Cloud Storage to work.');
+		// }
 
-		this.gcobj = storage({
-			projectId: opts.projectId,
-			keyFilename: opts.keyFilename
-		});
+		// this.gcobj = storage({
+		// 	projectId: opts.projectId,
+		// 	keyFilename: opts.keyFilename
+		// });
 
-		this.gcsBucket = this.gcobj.bucket(opts.bucket);
+		// this.gcsBucket = this.gcobj.bucket(opts.bucket);
 
-		this.options = opts;
+		// this.options = opts;
 	}
 
 	_handleFile = (req, file, cb) => {
-		this.getDestination(req, file, (err, destination) => {
+		// this.getDestination(req, file, (err, destination) => {
 
-			if (err) {
-				return cb(err);
-			}
+		// 	if (err) {
+		// 		return cb(err);
+		// 	}
 
-			this.getFilename(req, file, (err, filename) => {
-				if (err) {
-					return cb(err);
-				}
-				var gcFile = this.gcsBucket.file(filename);
+		// 	this.getFilename(req, file, (err, filename) => {
+		// 		if (err) {
+		// 			return cb(err);
+		// 		}
+		// 		var gcFile = this.gcsBucket.file(filename);
 
-				const streamOpts: Storage.WriteStreamOptions = {
-					predefinedAcl: this.options.acl || 'private'
-				};
+		// 		const streamOpts: Storage.WriteStreamOptions = {
+		// 			predefinedAcl: this.options.acl || 'private'
+		// 		};
 
-				const contentType = this.getContentType(req, file);
+		// 		const contentType = this.getContentType(req, file);
 
-				if (contentType) {
-					streamOpts.metadata = { contentType };
-				}
-				const stream = gcFile.createWriteStream(streamOpts);
-				file.stream.pipe(stream)
-					.on('data', (data) => {
-						ffmpeg(data)
-							.format('mp4')
-							.output(stream);
-					})
-					.on('error', (err) => cb(err))
-					.on('finish', (file) => cb(null, {
-						path: `https://${this.options.bucket}.storage.googleapis.com/${filename}`,
-						filename: filename
-					})
-					);
-			});
+		// 		if (contentType) {
+		// 			streamOpts.metadata = { contentType };
+		// 		}
+		// 		const stream = gcFile.createWriteStream(streamOpts);
+		// file.stream.pipe(stream)
+		// 	.on('data', (data) => {
+		// 		ffmpeg(data)
+		// 			.format('mp4')
+		// 			.output(stream);
+		// 		debugger;
+		// 	})
+		// 	.on('error', (err) => cb(err))
+		// 	.on('finish', (file) => cb(null, {
+		// 		path: `https://${this.options.bucket}.storage.googleapis.com/${filename}`,
+		// 		filename: filename
+		// 	}));
+		// 	});
 
-		});
+		// });
 	}
 	_removeFile = (req, file, cb) => {
-		var gcFile = this.gcsBucket.file(file.filename);
-		gcFile.delete();
+		// var gcFile = this.gcsBucket.file(file.filename);
+		// gcFile.delete();
 	};
 }
 
